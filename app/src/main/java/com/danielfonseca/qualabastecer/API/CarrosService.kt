431557d3f5184c17da38cfa2c4d9084407e74892
Daniel Fonseca <undeon@gmail.com>
2018-11-01 18:47:54 -0300

    Código parcialmente ok Primeira call OK Segunda call erro

--- app/src/main/java/com/danielfonseca/qualabastecer/API/CarrosService.kt
+++ app/src/main/java/com/danielfonseca/qualabastecer/API/CarrosService.kt
@@ -1,13 +1,19 @@
 package com.danielfonseca.qualabastecer.API
 
 import com.danielfonseca.qualabastecer.Model.Marca
+import com.danielfonseca.qualabastecer.Model.Modelo
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+
 
 interface CarrosService {
 
     @GET("carros/marcas.json")
-    fun recuperarMarca(): Call <List<Marca>>
+        fun recuperarMarca(): Call <List<Marca>>
 
+    @GET("{id}.json")
+        fun enviaIdSelecionado(@Path("id") id: String): Call<List<Marca>>
+        fun recuperarModelo(): Call <List<Modelo>>
 
 }
--- app/src/main/java/com/danielfonseca/qualabastecer/Veiculos/CadastrarVeiculosActivity.kt
+++ app/src/main/java/com/danielfonseca/qualabastecer/Veiculos/CadastrarVeiculosActivity.kt
@@ -2,6 +2,7 @@ package com.danielfonseca.qualabastecer.Veiculos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -10,6 +11,7 @@ import com.danielfonseca.qualabastecer.R
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.danielfonseca.qualabastecer.Model.Marca
+import com.danielfonseca.qualabastecer.Model.Modelo
 import kotlinx.android.synthetic.main.activity_cadastrar_veiculos.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -18,28 +20,35 @@ import retrofit2.Response
 
 class CadastrarVeiculosActivity : AppCompatActivity() {
 
-    var marcaEscolhida = "Escolha o Fabricante"
+    var marcaSelecionada = ""
+    var modeloSelecionado = ""
 
     var listaMarca = mutableListOf<Marca>()
+
+    var listaModelos = mutableListOf<Modelo>()
+
+    var idSelecionado = "-1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastrar_veiculos)
+
+        listaMarca.add(0, Marca("-1", "Escolha o Fabricante"))
 
         val retrofit = Retrofit.Builder()
                 .baseUrl("https://fipeapi.appspot.com/api/1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
-        val carroService = retrofit.create(CarrosService::class.java)
-        val call = carroService.recuperarMarca()
+        val carroServiceMarca = retrofit.create(CarrosService::class.java)
+        val call = carroServiceMarca.recuperarMarca()
 
         call.enqueue(object: Callback<List<Marca>>{
             override fun onResponse(call: Call<List<Marca>>?, response: Response<List<Marca>>?) {
                 if(response!!.isSuccessful()){
                     val marcas = response.body()
                     marcas.toString()
-                    listaMarca = marcas!!.toMutableList()
+                    listaMarca.addAll(marcas!!)
 
                     val aa = ArrayAdapter(this@CadastrarVeiculosActivity, android.R.layout.simple_spinner_item, listaMarca)
                     aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -51,8 +60,53 @@ class CadastrarVeiculosActivity : AppCompatActivity() {
                         }
 
                         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                            val idRetornado = listaMarca[position].id
-                            println(idRetornado)
+                            idSelecionado = listaMarca[position].id
+
+                            if(idSelecionado == "-1")
+                            {
+                                dropDownModelo.isEnabled = false
+                            }
+                            else {
+                                marcaSelecionada = listaMarca[position].fipe_name
+
+                                val retrofit = Retrofit.Builder()
+                                        .baseUrl("https://fipeapi.appspot.com/api/1/carros/veiculos/")
+                                        .addConverterFactory(GsonConverterFactory.create())
+                                        .build()
+
+                                val carroServiceModelo = retrofit.create(CarrosService::class.java)
+                                carroServiceModelo.enviaIdSelecionado(idSelecionado)
+                                val call = carroServiceModelo.recuperarModelo()
+
+                                call.enqueue(object : Callback<List<Modelo>> {
+                                    override fun onResponse(call: Call<List<Modelo>>?, response: Response<List<Modelo>>?) {
+                                        if (response!!.isSuccessful()) {
+                                            val modelos = response.body()
+                                            modelos.toString()
+                                            listaModelos = modelos!!.toMutableList()
+
+                                            val aa = ArrayAdapter(this@CadastrarVeiculosActivity, android.R.layout.simple_spinner_item, listaModelos)
+                                            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                                            dropDownModelo!!.setAdapter(aa)
+
+                                            dropDownModelo?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                                                override fun onNothingSelected(parent: AdapterView<*>?) {
+                                                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                                                }
+
+                                                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                                    modeloSelecionado = listaModelos[position].fipe_name
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    override fun onFailure(call: Call<List<Modelo>>?, t: Throwable?) {
+                                        call.toString()
+                                    }
+                                })
+                            }
+
                         }
                     }
                 }
